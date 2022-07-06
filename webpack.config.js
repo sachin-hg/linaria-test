ac8dd8a1687a962b07d7faded0caa3efe092dbbb
Dhruvil19 <dhruvilkpatidar@gmail.com>
2022-07-06 11:01:45 +0530

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: [/node_modules/, /\.linaria\.js/],
+        test: /\.jsx$/,
+        exclude: /node_modules/,
         use: [
           {
             loader: 'babel-loader',
